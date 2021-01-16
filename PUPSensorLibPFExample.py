87b2c7c09b51c7f7b12b70e7c95163963e9fa038
Tcm0 <32749263+Tcm0@users.noreply.github.com>
2021-01-16 18:42:23 +0100

    Add files via upload

---
+++ PUPSensorLibPFExample.py
@@ -0,0 +1,19 @@
+#A video of the example: https://www.youtube.com/watch?v=cxDJ34ofeHk
+from spike import ForceSensor
+from spike import PrimeHub
+myHub = PrimeHub()
+mySensor = colorDistanceSensor("F", 7)
+#You can use the following commands to send PF commands:
+#PFComboDirectCommand, PFSingleOutputCommand and PFComboPWMCommand
+force = ForceSensor('E')
+
+while True:
+    if force.is_pressed():
+        mySensor.PFComboDirectCommand(PFChannel.CHANNEL_4, PFMotor.BACKWARD, PFMotor.FORWARD)
+    elif myHub.left_button.is_pressed():
+        mySensor.PFComboDirectCommand(PFChannel.CHANNEL_4, PFMotor.BACKWARD, PFMotor.BACKWARD)
+    elif myHub.right_button.is_pressed():
+        mySensor.PFComboDirectCommand(PFChannel.CHANNEL_4, PFMotor.FORWARD, PFMotor.FORWARD)
+    else:
+        mySensor.PFComboDirectCommand(PFChannel.CHANNEL_4, PFMotor.FLOAT, PFMotor.FLOAT)
+    utime.sleep_ms(75)
